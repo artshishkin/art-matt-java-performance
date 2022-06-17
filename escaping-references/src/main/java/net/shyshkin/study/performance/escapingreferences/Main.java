5baf5b1a48d4d11a01048e480d0611cdcc60abe3
Art <d.art.shishkin@gmail.com>
2022-06-17 16:12:11 +0300

    28.2 Why we need escaping reference? - Problem (#6)

--- escaping-references/src/main/java/net/shyshkin/study/performance/escapingreferences/Main.java
+++ escaping-references/src/main/java/net/shyshkin/study/performance/escapingreferences/Main.java
@@ -1,5 +1,7 @@
 package net.shyshkin.study.performance.escapingreferences;
 
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,6 +14,10 @@ public class Main {
         for (Customer next : records.getCustomers().values()) {
             System.out.println(next);
         }
+
+        //Problem
+        Map<String, Customer> myCustomers = records.getCustomers();
+        myCustomers.clear(); //we do not want to allow this behaviour
 
     }
 
