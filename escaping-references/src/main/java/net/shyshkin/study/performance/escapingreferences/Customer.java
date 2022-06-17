2463cbc4482b3800b8dc97f944608930701a4c12
Art <d.art.shishkin@gmail.com>
2022-06-17 16:08:53 +0300

    28.1 Introduction - what is an escaping reference? (#6 Section 7: Chapter 7 - Escaping References)

---
+++ escaping-references/src/main/java/net/shyshkin/study/performance/escapingreferences/Customer.java
@@ -0,0 +1,18 @@
+package net.shyshkin.study.performance.escapingreferences;
+
+public class Customer {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+}
