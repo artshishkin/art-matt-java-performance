5bacec8490e1866bbbdd3cfaeb08c17f8e203b90
Art <d.art.shishkin@gmail.com>
2022-06-18 10:18:48 +0300

    41. Interning Strings (#8)

--- exploring-strings/src/main/java/net/shyshkin/study/performance/exploringstrings/Main.java
+++ exploring-strings/src/main/java/net/shyshkin/study/performance/exploringstrings/Main.java
@@ -11,15 +11,15 @@ public class Main {
         System.out.println(one == two);
         delimiter();
 
-        String three = one + two;
-        String four = one + two;
+        String three = (one + two).intern();
+        String four = (one + two).intern();
 
         System.out.println(three.equals(four));
         System.out.println(three == four);
         delimiter();
 
         Integer i = 76;
-        String five = i.toString();
+        String five = i.toString().intern();
         String six = "76";
         System.out.println(five.equals(six));
         System.out.println(five == six);
