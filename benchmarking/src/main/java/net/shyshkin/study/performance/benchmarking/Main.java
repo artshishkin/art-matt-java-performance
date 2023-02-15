6279d2f57c234db86ccc94813660b3ba2ca5daa8
Art <d.art.shishkin@gmail.com>
2023-02-15 15:03:35 +0200

    86. A simple approach to micro-benchmarking (#16)

--- benchmarking/src/main/java/net/shyshkin/study/performance/benchmarking/Main.java
+++ benchmarking/src/main/java/net/shyshkin/study/performance/benchmarking/Main.java
@@ -1,9 +1,19 @@
 package net.shyshkin.study.performance.benchmarking;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
         NumberChecker numberChecker = new NumberChecker();
-        System.out.println(numberChecker.isPrimeOriginal(22));
+
+        LocalDateTime start = LocalDateTime.now();
+        for (int i = 1001; i < 2000; i++) {
+            numberChecker.isPrimeOriginal(i);
+        }
+        LocalDateTime end = LocalDateTime.now();
+
+        System.out.println("Time taken: " + Duration.between(start, end));
     }
 }
