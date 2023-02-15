111ffffb99e494a6254cc22ada520af05297827f
Art <d.art.shishkin@gmail.com>
2023-02-15 15:43:55 +0200

    88. Comparing two code alternatives (#16)

--- benchmarking/src/main/java/net/shyshkin/study/performance/benchmarking/Main.java
+++ benchmarking/src/main/java/net/shyshkin/study/performance/benchmarking/Main.java
@@ -5,17 +5,20 @@ import java.time.LocalDateTime;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         NumberChecker numberChecker = new NumberChecker();
 
         //warm up period
         for (int i = 1; i < 10_000; i++)
-            numberChecker.isPrimeOriginal(i);
+            numberChecker.isPrimeOptimized(i);
+
+        Thread.sleep(2000);
+
         System.out.println("Warmup finished, now measuring");
 
         LocalDateTime start = LocalDateTime.now();
         for (int i = 1; i < 50_000; i++) {
-            numberChecker.isPrimeOriginal(i);
+            numberChecker.isPrimeOptimized(i);
         }
         LocalDateTime end = LocalDateTime.now();
 
