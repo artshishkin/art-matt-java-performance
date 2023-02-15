c3e7d99e9501ea9c5755efbd2fa610f56f0ad26a
Art <d.art.shishkin@gmail.com>
2023-02-15 15:32:46 +0200

    87. Adding in a warm-up period (#16)

--- benchmarking/src/main/java/net/shyshkin/study/performance/benchmarking/Main.java
+++ benchmarking/src/main/java/net/shyshkin/study/performance/benchmarking/Main.java
@@ -8,8 +8,13 @@ public class Main {
     public static void main(String[] args) {
         NumberChecker numberChecker = new NumberChecker();
 
+        //warm up period
+        for (int i = 1; i < 10_000; i++)
+            numberChecker.isPrimeOriginal(i);
+        System.out.println("Warmup finished, now measuring");
+
         LocalDateTime start = LocalDateTime.now();
-        for (int i = 1001; i < 2000; i++) {
+        for (int i = 1; i < 50_000; i++) {
             numberChecker.isPrimeOriginal(i);
         }
         LocalDateTime end = LocalDateTime.now();
